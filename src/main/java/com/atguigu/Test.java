2563b45c16134ecec6296ef47a64ae3a169a46e9
huang <577429849@qq.com>
2023-03-15 22:20:27 +0800

    创建ssh111

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test ssh");
+        System.out.println("test ssh111");
     }
 }
